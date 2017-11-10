93fe727b0855289aeac5be740da15d7f24eb9f06
Fabian Küppers <f.kueppers@hak-kitz.ac.at>
2017-11-10 10:45:45 +0100

    Login Ready

--- GitTestForm/GitTestForm/Form1.cs
+++ GitTestForm/GitTestForm/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace GitTestForm {
     public partial class Form1 : Form {
@@ -15,8 +16,31 @@ namespace GitTestForm {
         }
 
         private void button1_Click(object sender, EventArgs e) {
-            Form2 frm = new Form2(this);
-            frm.Show();
+            SqlConnection con = new SqlConnection("server = (localdb)\\MSSQLLocalDb; Integrated Security = yes; Database = Login;");
+
+            SqlCommand cmd = new SqlCommand("SELECT count (*) as cnt from Users where username=@usr and password=@pwd", con);
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@usr", textBox1.Text);
+            cmd.Parameters.AddWithValue("@pwd", textBox2.Text);
+            try
+            {
+                con.Open();
+                if (cmd.ExecuteScalar().ToString() == "1")
+                {
+                    MessageBox.Show("Logged In");
+                }
+                else
+                {
+                    MessageBox.Show("Failed");
+                }
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
         }
     }
 }
